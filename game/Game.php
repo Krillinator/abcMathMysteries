43a3ab696ab48c175b4c83718e070f4d00a9b6c4
kristoffer <kristoffer9464@hotmail.com>
2020-10-23 12:14:21 +0200

    Early prototype

--- game/Game.php
+++ game/Game.php
@@ -9,13 +9,45 @@ class Game extends MysteryGame
 {
   public function generateMystery()
   {
-    // Change this expression
+
+    $lettersTest =
+      [
+        "letters" => range("A", "Z"),
+        "value" => range("1", "26")
+      ];
+
+    $fusedletters = [];
+
+    for ($i = 0; $i < 25; $i++) {
+      $fusedletters[] = $lettersTest["letters"][$i] . $lettersTest["value"][$i];
+    }
+
+    shuffle($fusedletters);
+
+    // GET VALUE
+    $x = (int)substr($fusedletters[0], 1, 2);
+    $y = (int)substr($fusedletters[1], 1, 2);
+    $answer = $x + $y;
+
+    // MYSTERY 
+    $mystery =
+      substr($fusedletters[0], 0, 1) . ' + ' .
+      substr($fusedletters[1], 0, 1) . ' = ?';
+
     $this->mystery = [
-      "mystery" => "test",
-      "answer" => "test"
+      "mystery" => $mystery,
+      "answer" => $answer
     ];
   }
+
+  // HOW TO USE: findValue($fusedletters, "B")
+  // RETURNS: 2 (A = 1, B = 2, C = 3 etc..)
+  public function findValue($array, $letter)
+  {
+    for ($i = 0; $i < count($array); $i++) {
+      if ($letter == substr($array[$i], 0, 1)) { // 0 = letter .pos
+        return substr($array[$i], 1, 2); // Return value of letter
+      }
+    }
+  }
 }
-
-
-?>
